c5532c99bc4e2d33a769c45dd114f87324b30a8e
yzy613 <59520517+yzy613@users.noreply.github.com>
2025-01-08 13:38:36 +0800

    perf: clickhouse.optimizeTable.enableAfterInsert

--- internal/logic/cfg/clickhouse.go
+++ internal/logic/cfg/clickhouse.go
@@ -5,16 +5,16 @@ import (
 	"github.com/gogf/gf/v2/os/gcfg"
 )
 
-func (s *sCfg) IsClickHouseOptimizeTableAfterInsert(ctx context.Context) bool {
-	return gcfg.Instance().MustGet(ctx, "clickhouse.flush.enableOptimizeTableAfterInsert", false).Bool()
-}
-
 func (s *sCfg) ClickHouseCountFlush(ctx context.Context) uint {
 	return gcfg.Instance().MustGet(ctx, "clickhouse.flush.count", 0).Uint()
 }
 
 func (s *sCfg) ClickHouseCrontabFlush(ctx context.Context) string {
 	return gcfg.Instance().MustGet(ctx, "clickhouse.flush.crontab", "").String()
+}
+
+func (s *sCfg) IsClickHouseOptimizeTableAfterInsert(ctx context.Context) bool {
+	return gcfg.Instance().MustGet(ctx, "clickhouse.optimizeTable.enableAfterInsert", false).Bool()
 }
 
 func (s *sCfg) ClickHouseCrontabOptimizeTable(ctx context.Context) string {
